936f1bfa332e49f4ad08d92332dc175298772650
Philibert Rakotoasimbola <philibert@emoney-solutions.com>
2025-02-06 23:31:12 +0300

    Initial commit

---
+++ app/src/main/java/com/emoney/myappempy/Test.java
@@ -0,0 +1,8 @@
+package com.emoney.myappempy;
+
+import com.emoney.mylib1.Math;
+
+public class Test {
+    Math myMath = new Math();
+    int b = myMath.Add(1, 3);
+}
---
+++ mylib1/src/main/java/com/emoney/mylib1/Math.java
@@ -0,0 +1,9 @@
+package com.emoney.mylib1;
+
+public class Math {
+
+    public static int Add(int a, int b){
+        return a + b;
+    }
+
+}
